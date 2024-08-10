c56577bb727dfb93edee1564c33cd2a9856dc575
pppooowww <134120659+pppooowww@users.noreply.github.com>
2024-08-11 00:30:14 +0900

    1

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-const button = document.querySelector('button');
+window.addEventListener('scroll', function() {
+    const scrollMoveContainer = document.getElementById('scroll-move-container');
+    const introContainer = document.getElementById('intro-container');
+    const scrollPosition = window.scrollY;
 
-button.onclick = () => {
-    alert('클릭');
-}
+    // 스크롤 위치가 100px 이상일 때 이미지 나타남
+    if (scrollPosition > 200) {
+        scrollMoveContainer.classList.add('visible');
+        introContainer.classList.add('middle');
+        introContainer.style.left = '50%';
+        introContainer.style.transform = 'translate(-50%, 0)';
+    }
+});
+
+
